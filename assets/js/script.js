dbe4e1a3c4fccc73758ad16a1d47bde4d76088ed
MJGR <mjgriley@gmail.com>
2021-09-24 00:05:13 -0400

    data outputs to console now, just need to redirect to local storage and add check for previously stored values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,19 @@ var saveIcon = $('i')
 var textArea = $('textarea')
 var uInput = []
 
-saveIcon.each(function(i){
-    console.log(i)
-    //adds event listener to each saveIcon
-    var input = textArea[i]
-    console.log(input)
-    $(this).click( function(i){//This adds a listener to each saveIcon buttons
-        // and I want it to dynamically select the textArea, but it will only 
-        // select the first 
-        if(!temp){localStorage.setItem('hour'+i,JSON.stringify(textArea.val()))}
-    })
-})
+saveIcon.on('click',pushStorage)
 
 textArea.each(function(i){
     if (rightNow > i+9) {$(this).addClass('past')}
     if (rightNow == i+9) {$(this).addClass('present')}
     if (rightNow < i+9) {$(this).addClass('future')}
 })
+
+function pushStorage() {
+    $(this).parent().siblings('textarea').each(function(i){
+        uInput[i] = $(this).val()
+        //add push to local storage here
+    })
+}
+
+//add check for local storage when page loads "init"
