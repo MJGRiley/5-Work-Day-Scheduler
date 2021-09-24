4c3c610f7b87edc77cfbb0d702e3e52da11fb8a6
MJGR <mjgriley@gmail.com>
2021-09-24 19:49:33 -0400

    finished with the assignment, submitting

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# 5-Work-Day-Scheduler
+# Assignment 5-Work-Day-Scheduler
+    
+    User is presented with a schedule including 9 text inputs, one for each hour; date at the top. The text input fields change color with the hour to keep the user on track. This website will save the user's schedule they've input when the blue save icon is clicked. It maintains a copy on local storage incase they close the window and need to come back to it.
+
+* [Github repo](https://github.com/MJGRiley/5-Work-Day-Scheduler)
+* [Github pages](https://MJGRiley.github.io/5-Work-Day-Scheduler)
+
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
-
-var rightNow = moment().format("H")
+var rightNow = moment().format('H')
 var saveIcon = $('i')
 var textArea = $('textarea')
 var uInput = []
+// gets the date from momentjs
+var toDay = moment().format('dddd [the] Do [of] MMMM')
+//displays the date
+$('#currentDay').text(toDay)
 //adds a click listener for all of the saveIcon elements
 saveIcon.on('click', pushStorage)
-
+//changes the color of the textarea based on time +9 because the chart starts at 9am
 textArea.each(function(i){
     if (rightNow > i+9) {$(this).addClass('past')}
     if (rightNow == i+9) {$(this).addClass('present')}
     if (rightNow < i+9) {$(this).addClass('future')}
 })
-
+//this function is called when one of the save icons is clicked
 function pushStorage() {
+    //selects all textarea $(this) currently points at the saveIcon
     $(this).parent().siblings('textarea').each(function(i){
+        // S(this) changes to the textareas and puts the value of each into the user input array
         uInput[i] = $(this).val()
+        //sends the user input array to local storage after stringifying it
         localStorage.setItem('userInput',JSON.stringify(uInput))
     })
 }
 
 $(document).ready(function () {
+    //checks for any data currently in local storage
     var getCheck = JSON.parse(localStorage.getItem('userInput'))
-    if (!getCheck) {
+    if (!getCheck) { //if getCheck comes back null, kick it out of this function
         console.log('No data for this site stored on local storage.')
         return
     } else {
         for(i=0;i<getCheck.length;i++){
-        //getCheck.each(function(i){ //I don't understand why this doesn't work
+        //getCheck.each(function(i){     //I don't understand why this doesn't work
+        //is it because getCheck is an array of strings and not an array of DOM elements?
             textArea[i].value = getCheck[i]
         } 
     }
-
 })
